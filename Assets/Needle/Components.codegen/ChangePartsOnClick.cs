e9e97c4c20a9ee8316919af105694bf8cba231ea
diegodmag <diegodm@ciencias.unam.mx>
2024-04-15 23:11:56 -0600

    Initial Proyect

---
+++ Assets/Needle/Components.codegen/ChangePartsOnClick.cs
@@ -0,0 +1,14 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class ChangePartsOnClick : UnityEngine.MonoBehaviour
+	{
+		public void onPointerClick(UnityEngine.EventSystems.PointerEventData @args){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
---
+++ Assets/Needle/Components.codegen/PartController.cs
@@ -0,0 +1,15 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class PartController : UnityEngine.MonoBehaviour
+	{
+		public UnityEngine.GameObject @nextPart;
+		public void changePart(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
